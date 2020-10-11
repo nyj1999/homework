d729086a584a32ca285574726608ca7584cd83e3
NI <1528464853@qq.com>
2020-10-11 19:38:35 +0800

    Upload

---
+++ homework01/app/src/main/java/com/example/homework01/Marquee.java
@@ -0,0 +1,26 @@
+package com.example.homework01;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+
+public class Marquee extends AppCompatTextView {
+    public Marquee(Context context) {
+        super(context);
+    }
+
+    public Marquee(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Marquee(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public boolean isFocused() {
+        return true;
+    }
+}
+
